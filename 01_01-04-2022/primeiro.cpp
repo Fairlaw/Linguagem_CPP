610ebcc7bb6344818e4128f7532be1789a7338e5
Fairlaw <lucas29466@gmail.com>
2022-04-04 08:53:09 -0300

    Estudo c++

---
+++ 01_01-04-2022/primeiro.cpp
@@ -0,0 +1,13 @@
+//Programa para exibir uma mensagem. O código foi escrito em C++
+
+//Importante da biblioteca de entrada e saida de dados
+#include<iostream>
+
+int main()
+{
+    //saída padrão de dados
+    //cout(console out)-> Saída na tela de console(terminal)
+    std::cout <<"Bem Vindo ao C++\n";
+
+    return 0;
+}
---
+++ 01_01-04-2022/segundo+std.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n1 = 0;
+    int n2 = 0;
+    int n3 = 0;
+
+    cout <<"Entre com um número: ";
+    cin >> n1;
+    cout << "Entre com outro número: ";
+    cin >> n2;
+    n3 = n1 + n2;
+    cout << "O resultado é: "<< n3 << "\n";
+    return 0;
+}
