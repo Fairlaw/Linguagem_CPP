1a1bcc7d2dbbabddd75ae4d86f9e842f62f671cd
Fairlaw <lucas29466@gmail.com>
2022-04-04 11:08:22 -0300

    Criação da classe usuario

---
+++ 02_04-04-2022/usuario.h
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using std::string;
+using namespace std;
+
+class Usuario
+{
+    public:
+    string nome;
+    string senha;
+    string email;
+    string nivel;
+};
